9ef8f0e39f5b7897410946a07ce37131da42d616
Edmond <edmondthedev@gmail.com>
2024-09-18 07:47:22 -0700

    Add get todo by ID endpoint

--- app.js
+++ app.js
@@ -13,11 +13,46 @@ app.get('/todos', (req, res) => {
     res.json(todos);
 });
 
+// Get a specific todo by ID
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find(todo => todo.id === id);
+    if (todo) {
+        res.json(todo);
+    } else {
+        res.status(404).send('Todo not found');
+    }
+});
+
 // Add a new todo
 app.post('/todos', (req, res) => {
     const todo = req.body;
     todos.push(todo);
     res.status(201).json(todo);
+});
+
+// Update a specific todo by ID
+app.put('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const index = todos.findIndex(todo => todo.id === id);
+    if (index !== -1) {
+        todos[index] = { ...todos[index], ...req.body };
+        res.json(todos[index]);
+    } else {
+        res.status(404).send('Todo not found');
+    }
+});
+
+// Mark a specific todo as completed
+app.patch('/todos/:id/complete', (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find(todo => todo.id === id);
+    if (todo) {
+        todo.completed = true;
+        res.json(todo);
+    } else {
+        res.status(404).send('Todo not found');
+    }
 });
 
 // Delete a todo
